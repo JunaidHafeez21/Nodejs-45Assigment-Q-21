32cef11a7681705b81b5cab5496c3d74dc739786
JunaidHafeez21 <160257992+JunaidHafeez21@users.noreply.github.com>
2024-02-18 19:01:16 +0500

    Add files via upload

---
+++ Q21.js
@@ -0,0 +1,4 @@
+"use strict";
+// Q21: They think of something you could store in a TypeScript Object. Write a program that creates Objects containing these items
+let person = { name: "Junaid", fname: "Hafeez", age: 22 };
+console.log(person);
---
+++ Q21.ts
@@ -0,0 +1,5 @@
+// Q21: They think of something you could store in a TypeScript Object. Write a program that creates Objects containing these items
+
+let person: {name:"Junaid", fname:string, age:number} = {name: "Junaid", fname: "Hafeez",age: 22}
+
+console.log(person)
